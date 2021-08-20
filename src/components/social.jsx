91e6e6c1707fa0de0d1e6aee3623556272bea4a5
unknown <schmidt1519@gmail.com>
2021-08-19 22:01:28 -0500

    Added external links to all social media icons.

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { HStack } from '@chakra-ui/react';
+import {Icon, ExternalLinkIcon } from '@chakra-ui/icon';
+import { FaFacebook, FaTwitter, FaYoutube, FaSpotify, FaInstagramSquare } from 'react-icons/fa';
+import { Link } from "@chakra-ui/react"
+
 
 function Social() {
     return (
-        <div>
-
-        </div>
-    )
+        <HStack spacing="24">
+            <Link href="https://www.facebook.com/leighton.schmidt/" isExternal><Icon as={FaFacebook} boxSize="10"/></Link>
+            <Link href="https://twitter.com/Schmidt15" isExternal><Icon as={FaTwitter} boxSize="10"/></Link>
+            <Link href="https://www.youtube.com/channel/UCBxQk3QBpPZLkhhdsDoAl3w/" isExternal><Icon as={FaYoutube} boxSize="10"/></Link>
+            <Link href="https://open.spotify.com/user/schmidt1519/" isExternal><Icon as={FaSpotify} boxSize="10"/></Link>
+            <Link href="https://www.instagram.com/schmidt15/" isExternal><Icon as={FaInstagramSquare} boxSize="10"/></Link>
+        </HStack>
+    );
 }
 
 export default Social;
