471e2639ea7ce495fd7e76da9258bb7777b4f3f0
unknown <schmidt1519@gmail.com>
2021-08-19 21:32:17 -0500

    Updated Header component to include name, tagline, and picture. Added linear gradient color to name.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { VStack, Flex, Heading, IconButton, Spacer } from '@chakra-ui/react';
 import { FaSun, FaMoon, FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
 import { useColorMode } from '@chakra-ui/color-mode';
 import Profile from './components/profile';
 import Header from './components/header';
@@ -9,17 +10,21 @@ function App() {
 
   const {colorMode, toggleColorMode} = useColorMode();
   const isDark = colorMode === "dark";
-
+  
   return (
     <VStack p={5}>
       <Flex w="100%">
         {/* insert logo here */}
-        <Heading ml="6" size="md" fontWeight="semibold" color="blue.300">Leighton Schmidt</Heading>
+        <Heading ml="6" size="md" fontWeight="semibold" color="blue.300">L S</Heading>
         <Spacer></Spacer>
         <IconButton icon={<FaLinkedin />} isRound="true"></IconButton>
         <IconButton icon={<FaGithub />} isRound="true"></IconButton>
+        <IconButton icon={<FiMail />} isRound="true"></IconButton>
         <IconButton ml={8} icon={isDark ? <FaSun/> : <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
       </Flex>
+        <Header></Header>
+        <Social></Social>
+        <Profile></Profile>
     </VStack>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useColorMode } from '@chakra-ui/color-mode';
+import { useMediaQuery, Stack, Flex, Box, Text, Image } from '@chakra-ui/react';
 
 function Header() {
-    return (
-        <div>
 
-        </div>
+    const {colorMode} = useColorMode();
+    const isDark = colorMode === "dark";
+
+    const [isNotSmallScreen] = useMediaQuery("(min-width:600px)");
+
+    return (
+        <Stack>
+            <Flex direction={isNotSmallScreen ? "row" : "column"}
+                spacing="200px" p={isNotSmallScreen ? "32" : "0"}
+                alignSelf="flex-start">
+                <Box mt={isNotSmallScreen ? "0" : "16"} align="flex-start">
+                    <Text fontSize="5x1" fontWeight="semibold">Hello, I am</Text>
+                    <Text fontSize="7x1" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, teal.600)"
+                        bgClip="text">Leighton Schmidt</Text>
+                    <Text color={isDark ? "gray.200" : "gray.500"}>Developer. Husband. Father.</Text>
+                </Box>
+                <Image alignSelf="center" mt={isNotSmallScreen ? "0" : "12"}
+                    mb={isNotSmallScreen ? "0" : "12"} borderRadius="full"
+                    backgroundColor="transparent" boxShadow="lg"
+                    boxSize="300px" src="https://avatars.githubusercontent.com/u/82241251?v=4"/>
+                </Flex>
+        </Stack>
     )
 }
 
