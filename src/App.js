d42dcce485b8d8d4f293452684167dccf5f7f987
Schmidt1519 <schmidt1519@gmail.com>
2023-10-05 22:21:07 -0500

    Fixed projects server/client URL links.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,72 @@
-import { VStack, Flex, IconButton, Spacer, Link, Image, Box } from '@chakra-ui/react';
-import { FaSun, FaMoon, FaGithub, FaLinkedin } from 'react-icons/fa';
-import { FiMail } from 'react-icons/fi';
-import { useColorMode } from '@chakra-ui/color-mode';
-import Projects from './components/projects';
-import Header from './components/header';
-import Social from './components/social';
-import Skills from './components/skills';
-import navBar from './components/navBar';
-import './App.css';
-import {Switch, Route } from 'react-router-dom';
-
+import {
+  VStack,
+  Flex,
+  IconButton,
+  Spacer,
+  Link,
+  Image,
+  Box,
+} from "@chakra-ui/react";
+import { FaSun, FaMoon, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+import { useColorMode } from "@chakra-ui/color-mode";
+import Projects from "./components/projects";
+import Header from "./components/header";
+import Social from "./components/social";
+import Skills from "./components/skills";
+import navBar from "./components/navBar";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-
-  const {colorMode, toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
-  
+
   return (
     <div className="outer-div">
       <navBar />
       <VStack p={4}>
         <Flex w="100%">
           <Box>
-            <Image boxSize="60px" objectFit="cover" src="./images/ls-logo.png" alt="Leighton Schmidt logo"/>
+            <Image
+              boxSize="60px"
+              objectFit="cover"
+              src="./images/ls-logo.png"
+              alt="Leighton Schmidt logo"
+            />
           </Box>
           <Spacer></Spacer>
-          <Link href="https://www.linkedin.com/in/leighton-schmidt86/" isExternal><IconButton icon={<FaLinkedin />} isRound="true" mr="1"></IconButton></Link>
-          <Link href="https://github.com/Schmidt1519/" isExternal><IconButton icon={<FaGithub />} isRound="true" mr="1"></IconButton></Link>
-          <Link href="mailto:schmidt1519@gmail.com" isExternal> <IconButton icon={<FiMail />} isRound="true" type="button"></IconButton></Link>
-          <IconButton ml={8} icon={isDark ? <FaSun/> : <FaMoon />} isRound="true" onClick={toggleColorMode}></IconButton>
+          <Link
+            href="https://www.linkedin.com/in/leighton-schmidt86/"
+            isExternal
+          >
+            <IconButton
+              icon={<FaLinkedin />}
+              isRound="true"
+              mr="1"
+            ></IconButton>
+          </Link>
+          <Link href="https://github.com/Schmidt1519/" isExternal>
+            <IconButton icon={<FaGithub />} isRound="true" mr="1"></IconButton>
+          </Link>
+          <Link href="mailto:schmidt1519@gmail.com" isExternal>
+            {" "}
+            <IconButton
+              icon={<FiMail />}
+              isRound="true"
+              type="button"
+            ></IconButton>
+          </Link>
+          <IconButton
+            ml={8}
+            icon={isDark ? <FaSun /> : <FaMoon />}
+            isRound="true"
+            onClick={toggleColorMode}
+          ></IconButton>
         </Flex>
-          <Header></Header>
-          <Social ></Social>
+        <Header></Header>
+        <Projects></Projects>
+        <Social></Social>
       </VStack>
       <Switch>
         <Route path="/skills" component={Skills} />
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,88 +1,224 @@
-import React from 'react';
-import { useMediaQuery, Flex, Box, Text, Link, HStack, Stack } from '@chakra-ui/react';
-import './projects.css';
+import React from "react";
+import {
+  useMediaQuery,
+  Flex,
+  Box,
+  Text,
+  Link,
+  HStack,
+  Stack,
+} from "@chakra-ui/react";
+import "./projects.css";
 
 function Projects() {
+  const [isNotSmallScreen] = useMediaQuery("(min-width:600px)");
 
-    const [isNotSmallScreen] = useMediaQuery("(min-width:600px)");
+  return (
+    <Stack>
+      <Flex
+        direction={isNotSmallScreen ? "row" : "column"}
+        w="100%"
+        maxWidth={{ base: "100vh", md: "130vh", lg: "130vh", xl: "130vh" }}
+      >
+        <Box alignSelf="center" px="32" py="16">
+          <Text fontWeight="bold" fontSize="3xl" align="center">
+            Projects
+          </Text>
 
-    return (
-        <Stack>
-        <Flex direction={isNotSmallScreen ? "row" : "column"} w="100%" 
-            maxWidth={{ base: "100vh", md:"130vh", lg: "130vh", xl: "130vh" }} >
-        
-            <Box alignSelf="center" px="32" py="16">
-                <Text fontWeight="bold" fontSize="3xl" align="center">Projects</Text>
-            
-                <Flex direction={isNotSmallScreen ? "row" : "column"} mt={1}>
+          <Flex direction={isNotSmallScreen ? "row" : "column"} mt={1}>
+            <Flex
+              rounded="xl"
+              direction="column"
+              mt={2}
+              ml={isNotSmallScreen ? 4 : 0}
+              bg="blue.400"
+              h="20vh"
+              w="20vh"
+              justify="flex-end"
+              _hover={{ bg: "teal.400" }}
+            >
+              <Text
+                color="white"
+                p="4"
+                fontSize="xl"
+                fontWeight="semibold"
+                align="center"
+                margin="auto"
+              >
+                Med Spa
+              </Text>
+              <Flex alignSelf="center">
+                <Link
+                  href="https://github.com/Schmidt1519/med-spa-backend/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Server
+                  </Text>
+                </Link>
+                <Link
+                  href="https://github.com/Schmidt1519/med-spa-frontend/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Client
+                  </Text>
+                </Link>
+              </Flex>
+            </Flex>
 
-                    <Flex rounded="xl" direction="column" mt={2} ml={isNotSmallScreen ? 4 : 0}
-                    bg="blue.400" h="20vh" w="20vh" justify="flex-end" _hover={{ bg: "teal.400", }}>
-                        <Text color="white" p="4" fontSize="xl" fontWeight="semibold" align="center" margin="auto">Med Spa</Text>
-                            <Flex alignSelf="center">
-                                <Link href="https://github.com/Schmidt1519/med-spa-frontend/" isExternal>
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold" >Server</Text>
-                                </Link>
-                                <Link href="https://github.com/Schmidt1519/med-spa-backend/" isExternal>
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold">Client</Text>
-                                </Link>
-                            </Flex>
-                    </Flex>
+            <Flex
+              rounded="xl"
+              direction="column"
+              mt={2}
+              ml={isNotSmallScreen ? 4 : 0}
+              bg="blue.400"
+              h="20vh"
+              w="20vh"
+              justify="flex-end"
+              _hover={{ bg: "teal.400" }}
+            >
+              <Text
+                color="white"
+                p="4"
+                fontSize={isNotSmallScreen ? "xl" : "md"}
+                fontWeight="semibold"
+                align="center"
+                margin="auto"
+              >
+                Star Wars E-Commerce
+              </Text>
+              <Flex alignSelf="center">
+                <Link
+                  href="https://github.com/Schmidt1519/StarWarsECommerce/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Server/Client
+                  </Text>
+                </Link>
+              </Flex>
+            </Flex>
 
-                    <Flex rounded="xl" direction="column" mt={2} ml={isNotSmallScreen ? 4 : 0}
-                        bg="blue.400" h="20vh" w="20vh" justify="flex-end" _hover={{ bg: "teal.400", }}>
-                        <Text color="white" p="4" fontSize={isNotSmallScreen ? "xl" : "md"} fontWeight="semibold" 
-                            align="center" margin="auto">Star Wars E-Commerce</Text>
-                            <Flex alignSelf="center">
-                            <Link href="https://github.com/Schmidt1519/StarWarsECommerce/" isExternal>
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold">Server/Client</Text>
-                                </Link>
-                            </Flex>
-                    </Flex>
+            <Flex
+              rounded="xl"
+              direction="column"
+              mt={2}
+              ml={isNotSmallScreen ? 4 : 0}
+              bg="blue.400"
+              h="20vh"
+              w="20vh"
+              justify="flex-end"
+              _hover={{ bg: "teal.400" }}
+            >
+              <Text
+                color="white"
+                p="4"
+                fontSize="xl"
+                fontWeight="semibold"
+                align="center"
+                margin="auto"
+              >
+                Flashcards
+              </Text>
+              <Flex alignSelf="center">
+                <Link
+                  href="https://github.com/Schmidt1519/flashcard-django-rest/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Server
+                  </Text>
+                </Link>
+                <Link
+                  href="https://github.com/Schmidt1519/flashcards-react/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Client
+                  </Text>
+                </Link>
+              </Flex>
+            </Flex>
 
-                    <Flex rounded="xl" direction="column" mt={2} ml={isNotSmallScreen ? 4 : 0}
-                    bg="blue.400" h="20vh" w="20vh" justify="flex-end" _hover={{ bg: "teal.400", }}>
-                        <Text color="white" p="4" fontSize="xl" fontWeight="semibold" align="center" margin="auto">Flashcards</Text>
-                            <Flex alignSelf="center">
-                            <Link href="https://github.com/Schmidt1519/flashcard-react/" isExternal>
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold">Server</Text>
-                                </Link>
-                                <Link href="https://github.com/Schmidt1519/flashcards-django-rest/" isExternal>
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold">Client</Text>
-                                </Link>
-                            </Flex>
-                    </Flex>
+            <Flex
+              rounded="xl"
+              direction="column"
+              mt={2}
+              ml={isNotSmallScreen ? 4 : 0}
+              bg="blue.400"
+              h="20vh"
+              w="20vh"
+              justify="flex-end"
+              _hover={{ bg: "teal.400" }}
+            >
+              <Text
+                color="white"
+                p="4"
+                fontSize="xl"
+                fontWeight="semibold"
+                align="center"
+                margin="auto"
+              >
+                Music Library
+              </Text>
+              <Flex alignSelf="center">
+                <Link
+                  href="https://github.com/Schmidt1519/music-library/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Server
+                  </Text>
+                </Link>
+                <Link
+                  href="https://github.com/Schmidt1519/music-library-react/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Client
+                  </Text>
+                </Link>
+              </Flex>
+            </Flex>
 
-                    <Flex rounded="xl" direction="column" mt={2} ml={isNotSmallScreen ? 4 : 0}
-                    bg="blue.400" h="20vh" w="20vh" justify="flex-end" _hover={{ bg: "teal.400", }} >
-                        <Text color="white" p="4" fontSize="xl" fontWeight="semibold" align="center" margin="auto">Music Library</Text>
-                            <Flex alignSelf="center">
-                            <Link href="https://github.com/Schmidt1519/music-library-react/" isExternal >
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold">Server</Text>
-                                </Link>
-                                <Link href="https://github.com/Schmidt1519/music-library/" isExternal>
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold">Client</Text>
-                                </Link>
-                            </Flex>
-                    </Flex>
-
-                    <Flex rounded="xl" direction="column" mt={2} ml={isNotSmallScreen ? 4 : 0}
-                        bg="blue.400" h="20vh" w="20vh" justify="flex-end" _hover={{ bg: "teal.400", }}>
-                        <Text color="white" p="4" fontSize="xl" fontWeight="semibold" align="center" margin="auto">Trash Collector</Text>
-                            <Flex alignSelf="center">
-                            <Link href="https://github.com/Schmidt1519/trash-collector/" isExternal>
-                                    <Text color="white" p="4" fontSize="sm" fontWeight="semibold">Server/Client</Text>
-                                </Link>
-                            </Flex>
-                    </Flex>
-
-                </Flex>
-     
-            </Box>
-
-        </Flex>
-        </Stack>
-        );
+            <Flex
+              rounded="xl"
+              direction="column"
+              mt={2}
+              ml={isNotSmallScreen ? 4 : 0}
+              bg="blue.400"
+              h="20vh"
+              w="20vh"
+              justify="flex-end"
+              _hover={{ bg: "teal.400" }}
+            >
+              <Text
+                color="white"
+                p="4"
+                fontSize="xl"
+                fontWeight="semibold"
+                align="center"
+                margin="auto"
+              >
+                Trash Collector
+              </Text>
+              <Flex alignSelf="center">
+                <Link
+                  href="https://github.com/Schmidt1519/trash-collector/"
+                  isExternal
+                >
+                  <Text color="white" p="4" fontSize="sm" fontWeight="semibold">
+                    Server/Client
+                  </Text>
+                </Link>
+              </Flex>
+            </Flex>
+          </Flex>
+        </Box>
+      </Flex>
+    </Stack>
+  );
 }
 
 export default Projects;
